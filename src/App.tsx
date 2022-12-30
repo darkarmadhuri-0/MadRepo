35c978a27ddbba635018fe1da0f59f62e977c7b3
Madhuri Darkar <madhuri_darkar@gmail.com>
2022-12-30 16:14:35 +0530

    Code Pushed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes , Route } from "react-router-dom";
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { HelpCenter } from './pages/HelpCenter';
+import { Navbar } from './components/Navbar';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
+import {RecoilRoot} from 'recoil'
+import BuyNow from './components/BuyNow';
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+    <ShoppingCartProvider>
+      <Navbar/>
+    
+      <Routes>
+        
+        <Route path='/' element={<Home/>}></Route>    {/*home component is rendered} */}
+        <Route path='/store' element={<Store/>}></Route>       
+        <Route path='/about' element={<HelpCenter/>}></Route>
+        <Route path='/components/BuyNow' element={<BuyNow/>}></Route>
+      </Routes>
+   
+    </ShoppingCartProvider>
+    </RecoilRoot>
   );
 }
 
